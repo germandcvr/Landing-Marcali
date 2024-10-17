2821e8fb452a947c60bef93c2f5e75008c67f74b
germandcvr <gcelis@vereon.co>
2024-10-17 12:17:49 -0500

    [Conf] Configuración astro y prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+		inlineStylesheets: "always",
+	},
+	compressHTML: true,
+  devToolbar: {
+		enabled: false,
+	},
+  integrations: [tailwind()],
+  vite: {
+    build: {
+      cssMinify: "lightningcss",
+    },
+  }
+});
