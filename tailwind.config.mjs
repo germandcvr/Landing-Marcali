bfb128fe89576fdb0fc90fc32d00d8a02066ea63
germandcvr <gcelis@vereon.co>
2024-10-18 16:25:07 -0500

    Creando hero section adaptado para movil y desktop

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primero': '#000047',
+				'segundo': '#ffdf5a',
+				'tercero': '#e5e5e5'
+			},
+			backgroundImage: {
+				"hero-car": "url('/banner-hero-fondo.webp')",
+			}
+		},
 	},
 	plugins: [],
 }
